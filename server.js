cb58b1ae72f20c661a7518d5da7b0c8d83f25463
wschilpz <warren.schilpzand@gmail.com>
2016-05-14 09:00:18 +1000

    remove uneeded from server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    fs      = require('fs'),
-    app     = express(),
-    eps     = require('ejs'),
-    morgan  = require('morgan');
+    fs = require('fs'),
+    app = express(),
+    eps = require('ejs'),
+    morgan = require('morgan');
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
-
+/*
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
       mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
@@ -55,41 +55,45 @@ var initDb = function(callback) {
     console.log('Connected to MongoDB at: %s', mongoURL);
   });
 };
-
+*/
 app.get('/', function (req, res) {
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    /* if (db) {
+       var col = db.collection('counts');
+       // Create a document with request IP and current time of request
+       col.insert({ip: req.ip, date: Date.now()});
+       col.count(function(err, count){
+         res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+       });
+     } else {
+       res.render('index.html', { pageCountMessage : null});
+     }*/
+    res.render('index.html', {
+        pageCountMessage: null
     });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
 });
 
 app.get('/pagecount', function (req, res) {
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
+    /*  if (db) {
+        db.collection('counts').count(function(err, count ){
+          res.send('{ pageCount: ' + count + '}');
+        });
+      } else {
+        res.send('{ pageCount: -1 }');
+      }*/
     res.send('{ pageCount: -1 }');
-  }
 });
 
 // error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something bad happened!');
 });
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
+/*initDb(function (err) {
+    console.log('Error connecting to Mongo. Message:\n' + err);
+});*/
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
-module.exports = app ;
+module.exports = app;
